287bf835e70dab7afd87d7c34899f7382f36429a
Pablo Anchondo Hernandez <pablo.anchondo@soisa.int>
2026-03-20 09:17:06 -0600

    fix/Mostrar espacio de publicidad unicamente si carga

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { View } from "react-native";
-import { BannerAd, BannerAdSize } from "react-native-google-mobile-ads";
+import {
+  BannerAd,
+  BannerAdSize
+} from "react-native-google-mobile-ads";
 
 export default function AdBanner() {
   // const adUnitId = TestIds.BANNER;
   const adUnitId = "ca-app-pub-8421586383850459/6275001671";
 
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
   return (
     <View
       style={{
         alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        height: isLoaded ? undefined : 0,
+        overflow: "hidden",
       }}
     >
-      <BannerAd unitId={adUnitId} size={BannerAdSize.BANNER} />
+      <BannerAd
+        unitId={adUnitId}
+        size={BannerAdSize.LARGE_ANCHORED_ADAPTIVE_BANNER}
+        onAdLoaded={() => {
+          console.log("Ad loaded");
+          setIsLoaded(true);
+        }}
+        onAdFailedToLoad={(error) => {
+          console.log("Ad failed:", error);
+        }}
+      />
     </View>
   );
 }
